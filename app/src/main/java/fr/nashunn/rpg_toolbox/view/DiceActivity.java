7332905d35664014d86ed43bdc08735fd4f7f442
Nicolas B <boullet.nicolas@gmail.com>
2019-11-25 15:00:23 +0100

    add Activity dice

--- app/src/main/java/fr/nashunn/rpg_toolbox/view/DiceActivity.java
+++ app/src/main/java/fr/nashunn/rpg_toolbox/view/DiceActivity.java
@@ -1,4 +1,16 @@
 package fr.nashunn.rpg_toolbox.view;
 
-public class DiceActivity {
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import fr.nashunn.rpg_toolbox.R;
+
+public class DiceActivity extends AppCompatActivity {
+
 }
